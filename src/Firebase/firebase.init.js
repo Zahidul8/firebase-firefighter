5e3f9573e45adca0c985724c8280657003fe4901
Your NameZahidul8 <www.zahidul8@gmail.com>
2025-10-17 09:32:28 +0600

    hide our confidential data

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6VCh5410gbO4RXOPF4bJeWyTUEe3VCSE",
-  authDomain: "fir-firefighter-b1001.firebaseapp.com",
-  projectId: "fir-firefighter-b1001",
-  storageBucket: "fir-firefighter-b1001.firebasestorage.app",
-  messagingSenderId: "626699620687",
-  appId: "1:626699620687:web:a8b0a3496cb9d582b1753a"
+  apiKey:import.meta.env.VITE_apiKey ,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId ,
 };
 
 // Initialize Firebase
